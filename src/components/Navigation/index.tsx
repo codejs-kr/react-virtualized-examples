543de7001c4a499cdc904b83eb3ae641e590bf37
dodortus <dodortus@gmail.com>
2021-07-23 18:29:55 +0900

    chore: 문구수정

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -5,17 +5,17 @@ import './index.scss';
 const Naviagation = () => {
   return (
     <SimpleGrid className="navigation" columns={2} spacingX="5px" spacingY="20px">
-      <NavLink to="/text-list" activeClassName="selected">
-        텍스트 목록
-      </NavLink>
       <NavLink to="/text-list-virtualized" activeClassName="selected">
         텍스트 목록 (with virtualized)
       </NavLink>
-      <NavLink to="/image-list" activeClassName="selected">
-        이미지 목록
+      <NavLink to="/text-list" activeClassName="selected">
+        텍스트 목록 (normal)
       </NavLink>
       <NavLink to="/image-list-virtualized" activeClassName="selected">
         이미지 목록 (with virtualized)
+      </NavLink>
+      <NavLink to="/image-list" activeClassName="selected">
+        이미지 목록 (normal)
       </NavLink>
     </SimpleGrid>
   );
